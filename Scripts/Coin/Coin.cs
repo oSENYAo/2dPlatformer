0b26a4462db360bc5bf02a6c39b96a597de10f4a
Arsen Kuvatow <kuv99@mail.ru>
2022-12-26 18:31:45 +0300

    Update Coin.cs

--- Scripts/Coin/Coin.cs
+++ Scripts/Coin/Coin.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Coin : MonoBehaviour
 {
-    public bool IsDestroy { get; private set; }
-
-    private void Awake()
-    {
-        IsDestroy = false;
-    }
+    public UnityAction Destroyed;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Destroyed?.Invoke();
         Destroy(gameObject);
-        IsDestroy = true;
     }
 }
